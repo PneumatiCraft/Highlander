8fca01993ed32f661ccdf9b5a3b7e62cbd73603b
Eric Stokes <fernferret@gmail.com>
2011-09-20 20:24:41 -0600

    Initial Commit

---
+++ src/main/java/com/fernferret/TopPlayers/TopPlayers.java
@@ -0,0 +1,13 @@
+package com.fernferret.TopPlayers;
+import com.pneumaticraft.commandhandler;
+import org.bukkit.plugin.java.JavaPlugin;
+public class TopPlayers extends JavaPlugin {
+    private CommandHandler ch;
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
